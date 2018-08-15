02424ab7316f5c7583904cb65f7521e148061ba2
michael cockcroft <michael.cockcroft2@sky.uk>
2018-08-15 18:35:37 +0100

    More styling fixes

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       score_filter: 0
     }
 
-    this.updateGenreFilter = this.updateGenreFilter.bind(this);
+    this.clickCheckBox = this.clickCheckBox.bind(this);
     this.updateScoreFilter = this.updateScoreFilter.bind(this);
   }
 
@@ -33,7 +33,7 @@ class App extends Component {
     return genreIds.map(id => {
       const label = genres.find(genre => genre.id === id);
       return (
-        <span>
+        <span className="movies__genre-label">
           {label.name}
         </span>
       )
@@ -46,14 +46,20 @@ class App extends Component {
     });
   };
 
-  updateGenreFilter(event) {
+  clickCheckBox(event) {
+    const checkbox = event.target.children[1];
     const filters = this.state.genre_filter;
+    const classes = event.target.classList;
 
-    if (event.target.checked) {
-      filters.push(event.target.value)
+    checkbox.checked = !checkbox.checked;
+
+    if (checkbox.checked) {
+      filters.push(checkbox.value);
+      event.target.classList.add('filters__genre--selected');
     } else {
-      const index = filters.indexOf(event.target.value)
-      filters.splice(index, 1)
+      const index = filters.indexOf(checkbox.value);
+      filters.splice(index, 1);
+      event.target.classList.remove('filters__genre--selected');
     };
 
     this.setState({
@@ -73,30 +79,42 @@ class App extends Component {
     const { genres, movies } = this.state;
 
     return (
-      <div className="App">
-        Rating
-        <input type="range" min="0" max="10" step="0.5" value={this.state.score_filter} onChange={this.updateScoreFilter} />
-        Genres
-        {genres && genres.map((genre, key) => {
-          return (
-            <div key={key}>
-              <label htmlFor={`genre-${genre.id}`}>{genre.name}</label>
-              <input onChange={this.updateGenreFilter} type="checkbox" id={`genre-${genre.id}`} value={genre.id} />
-            </div>
-          )
-        })}
+      <main className="app">
+        <section className="filters">
+          <label htmlFor="rating"><h2>Rating</h2></label>
+          <div className="filters__rating">
+            <input id="rating" type="range" min="0" max="10" step="0.5" value={this.state.score_filter} onChange={this.updateScoreFilter} />
+          </div>
+          <h2>Genres</h2>
+          <div className="filters__genres">
+            {genres && genres.map((genre, key) => {
+              return (
+                <div className="filters__genre" onClick={this.clickCheckBox} key={key}>
+                  <label htmlFor={`genre-${genre.id}`}>{genre.name}</label>
+                  <input type="checkbox" id={`genre-${genre.id}`} value={genre.id} />
+                </div>
+              )
+            })}
+          </div>
+        </section>
 
-        Movies
-        {movies && this.filterMovies(movies).map((movie, key) => {
-          return (
-            <ul key={key}>
-              <li><img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`${movie.title}-image`} /></li>
-              <li>{movie.title}</li>
-              <li>{this.getGenreLabels(movie.genre_ids)}</li>
-            </ul>
-          )
-        })}
-      </div>
+        <section class="movies"> 
+          <h2>Movies</h2>
+          <div className="movies__results">
+            {movies && this.filterMovies(movies).map((movie, key) => {
+              return (
+                <article className="movies__result" key={key}>
+                  <div className="movies__image"><img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={`${movie.title}-image`} /></div>
+                  <div>
+                    <div className="movies__title"><h3>{movie.title}</h3></div>
+                    <div className="movies__genre-labels">{this.getGenreLabels(movie.genre_ids)}</div>
+                  </div>
+                </article>
+              )
+            })}
+          </div>
+        </section>
+      </main>
     );
   }
 }
